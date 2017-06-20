41e5a5bcd00b8bd4b83b057fcc6ab9247acf6ed0
Lustosa <angelolustosa@gmail.com>
2017-06-20 09:48:16 -0300

    Topic Controller, with get and get with id

---
+++ src/main/java/com/course/springbootstarter/topic/TopicController.java
@@ -0,0 +1,33 @@
+package com.course.springbootstarter.topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
+	
+	@RequestMapping("/topics")
+	/*public String listAllTopics() {
+		return "All topics";
+	}*/
+	
+	public List<Topic> listAllTopics() {
+		return topicService.getAllTopics();/*Arrays.asList(
+				new Topic("spring",  "Spring Framework", "Spring Framework Description"),
+				new Topic("java",  "Core Java", "Core Java Description"),
+				new Topic("javascript",  "Javascript", "Javascript Description")
+				);*/
+	}
+	@RequestMapping("/topics/{id}")
+	public Topic getTopicById(@PathVariable String id) {
+		return topicService.getTopicById(id);
+	}
+
+}
