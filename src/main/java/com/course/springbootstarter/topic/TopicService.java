dab78939d62d61799841668a5f5d10e3383aa0e0
Lustosa <angelolustosa@gmail.com>
2017-06-20 10:11:38 -0300

    Post topic with arrayList imutable

--- src/main/java/com/course/springbootstarter/topic/TopicService.java
+++ src/main/java/com/course/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.course.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	//imutable list
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring",  "Spring Framework", "Spring Framework Description"),
 			new Topic("java",  "Core Java", "Core Java Description"),
 			new Topic("javascript",  "Javascript", "Javascript Description")
-			);
+			));
 	
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -21,6 +23,11 @@ public class TopicService {
 	public Topic getTopicById(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 		
+	}
+
+	public void addTopic(Topic topic) {
+		// TODO Auto-generated method stub
+		topics.add(topic);
 	}
 
 }
